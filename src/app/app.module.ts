29b85875138f38623ab10b571ba2509cbf6dc275
lakindu hewawasam <lakindu2002@live.com>
2021-10-29 23:20:52 +0530

    completed article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent,
-    ContactUsComponent,
-    SignUpComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
